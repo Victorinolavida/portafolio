49db11b213d025df3078d7a851762c03e42c5c74
victorinoLavida <victorinolavida@ciencias.unam.mx>
2024-04-22 16:44:02 -0600

    feat: add basic information

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Portafolio de Victoirino Lavida';
+export const SITE_DESCRIPTION = 'Portafolio de Victoirino Lavida';
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/victorino-ruiz/';
+export const GITHUB_LINK = 'https://github.com/victorinolavida';
+
