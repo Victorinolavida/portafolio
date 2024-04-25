182d1dce73f003b2c3fccb6389c1db27f8b5a894
victorinoLavida <victorinolavida@ciencias.unam.mx>
2024-04-24 19:24:42 -0600

    style: improve styles in introduction section

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Portafolio de Victoirino Lavida';
-export const SITE_DESCRIPTION = 'Portafolio de Victoirino Lavida';
-export const LINKEDIN_LINK = 'https://www.linkedin.com/in/victorino-ruiz/';
-export const GITHUB_LINK = 'https://github.com/victorinolavida';
-
+export const SITE_TITLE = 'Portafolio de Victoirino Lavida'
+export const SITE_DESCRIPTION = 'Portafolio de Victoirino Lavida'
+export const LINKEDIN_LINK = 'https://www.linkedin.com/in/victorino-ruiz/'
+export const GITHUB_LINK = 'https://github.com/victorinolavida'
--- src/content/data.ts
+++ src/content/data.ts
@@ -1,41 +1,44 @@
-export const WORK_EXPERIENCE = [ {
-      "location": "Monterrey",
-      "position": "Jr full stack developer",
-      "website": "",
-      "startDate": "Jan 2024",
-      "endDate": "Present",
-      "highlights": [
-        "Make a landing page for attraction new users",
-        "Make reports with SQL query in PostgreSQL",
-        "Integrating new creating account flow ",
-        "Improving user experience making a search functionality adding filters by distinct categories."
-      ],
-      "company": "Strade"
-    },
-    {
-      "highlights": [
-        "Making a new inventory app for equipment and users",
-        "Deploying project with Gitlab runners and a GCP instance",
-        "Migrating a database from Mysql to PostgreSQL",
-        "Deploying a new internal database with PostgreSQL and Docker "
-      ],
-      "company": "Konfio",
-      "position": "Jr IT Engineer",
-      "location": "Mexico City",
-      "startDate": "Jun 2022",
-      "endDate": "Jan 2024"
-    },
-    {
-      "highlights": [
-        "Provide helpdesk for company internal users",
-        "Troubleshoot for Windows and macOs systems",
-        "Utilized Jira to manage and resolve tickets",
-        "Develop automation scripts in Python"
-      ],
-      "company": "Konfio",
-      "position": "IT Intern",
-      "location": "Mexico City",
-      "startDate": "Jul 2022",
-      "endDate": "Jun 2023"
-    }
-  ]
+export const WORK_EXPERIENCE = [
+  {
+    location: 'Monterrey',
+    position: 'Jr full stack developer',
+    website: '',
+    startDate: 'Jan 2024',
+    endDate: 'Present',
+    highlights: [
+      'Make a landing page for attraction new users',
+      'Make reports with SQL query in PostgreSQL',
+      'Integrating new creating account flow ',
+      'Improving user experience making a search functionality adding filters by distinct categories.',
+    ],
+    company: 'Strade',
+  },
+  
+
+  {
+    highlights: [
+      'Making a new inventory app for equipment and users',
+      'Deploying project with Gitlab runners and a GCP instance',
+      'Migrating a database from Mysql to PostgreSQL',
+      'Deploying a new internal database with PostgreSQL and Docker ',
+    ],
+    company: 'Konfio',
+    position: 'Jr IT Engineer',
+    location: 'Mexico City',
+    startDate: 'Jun 2022',
+    endDate: 'Jan 2024',
+  },
+  {
+    highlights: [
+      'Provide helpdesk for company internal users',
+      'Troubleshoot for Windows and macOs systems',
+      'Utilized Jira to manage and resolve tickets',
+      'Develop automation scripts in Python',
+    ],
+    company: 'Konfio',
+    position: 'IT Intern',
+    location: 'Mexico City',
+    startDate: 'Jul 2022',
+    endDate: 'Jun 2023',
+  },
+]
